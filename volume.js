59fd998ed40a2b40292eceefc3e8bf7f36b7d3ec
SonyWartono <127180068+SonyWartono@users.noreply.github.com>
2023-09-22 09:26:50 +0800

    Add files via upload

---
+++ volume.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hitungVolBtn = document.getElementById("hitungVol");
+    const hasilVol = document.getElementById("hasilVol");
+
+    hitungVolBtn.addEventListener("click", () => {
+        const panjang = parseFloat(document.getElementById("panjang").value);
+        const lebar = parseFloat(document.getElementById("lebar").value);
+        const tinggi = parseFloat(document.getElementById("tinggi").value);
+
+        if (isNaN(panjang) || isNaN(lebar) || isNaN(tinggi) || panjang <= 0 || lebar <= 0 || tinggi <= 0) {
+            hasilVol.textContent = "Masukkan panjang, lebar, dan tinggi yang valid.";
+        } else {
+            const volume = panjang * lebar * tinggi; // Menghitung volume cuboid
+            hasilVol.textContent = `Volume Cuboid: ${volume} cm`;
+        }
+    });
+});
